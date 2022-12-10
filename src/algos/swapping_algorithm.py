34330d621deabf4b28eec60bb5aad1af93a54b2d
Wojtek Seweryn <wseweryn@student.agh.edu.pl>
2022-12-10 01:18:00 +0100

    restructure

--- src/algos/swapping_algorithm.py
+++ src/algos/swapping_algorithm.py
@@ -5,6 +5,9 @@ from ..utils import StopAlgorithm
 
 
 class SwappingAlgorithm(Algorithm):
+    DISTANCE = 'distance'
+    SWAP = 'swap'
+
     def __init__(self,
                  neigh_type: str = None,
                  n_iter: int = 30,
@@ -59,3 +62,44 @@ class SwappingAlgorithm(Algorithm):
         # start new iteration
         self._i += 1
         return best_distance, best_swap
+
+    def _swap_elements(self, swap: tuple) -> list:
+        """ Returns copy of the current path with swapped indices """
+        path = self._path.copy()
+        pos1 = path.index(swap[0])
+        pos2 = path.index(swap[1])
+        path[pos1], path[pos2] = path[pos2], path[pos1]
+        return path
+
+    def _find_best_swap(self,
+                        swaps: List[tuple],
+                        distances: pd.DataFrame
+                        ) -> pd.Series:
+        distances_df = self._get_swaps_df(swaps=swaps, distances=distances)
+        # taking row of the best swaps
+        best_swap = distances_df.iloc[0]
+        return best_swap
+
+    def _get_swaps_df(self,
+                      swaps: List[tuple],
+                      distances: pd.DataFrame
+                      ) -> pd.DataFrame:
+        # new paths with swapped order
+        new_paths = list(map(lambda x: self._swap_elements(swap=x), swaps))
+        # distances of all possible new paths
+        paths_distances = list(map(lambda x: self._get_path_distance(
+            distances=distances, path=x), new_paths))
+        # DataFrame of all swaps and distances of formed paths
+        distances_df = pd.DataFrame({self.SWAP: swaps, self.DISTANCE: paths_distances})
+        # sorting distances in ascending order
+        distances_df = distances_df.sort_values(self.DISTANCE)
+        return distances_df
+
+    def _get_all_swaps(self, indices: list) -> List[tuple]:
+        """ Returns all possible swaps of indices """
+        index = len(indices) + 1
+        # unique combination
+        swaps = [(x, y) for x in range(1, index)
+                 for y in range(1, index)
+                 if (x != y) and (y > x)]
+        return swaps
