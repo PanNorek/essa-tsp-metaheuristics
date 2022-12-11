986333e12d83846e16c4d18e9918d7f46c1d324d
Rafal Nojek <dornier1997@gmail.com>
2022-12-11 15:40:54 +0100

    add github actions files

--- src/utils/time_it.py
+++ src/utils/time_it.py
@@ -3,9 +3,9 @@ import time
 
 def time_it(func):
     def wrapper(*args, **kwargs):
-        tic = time.time()
+        tic = time.perf_counter()
         result = func(*args, **kwargs)
-        toc = time.time()
+        toc = time.perf_counter()
         result.time = toc-tic
         return result
     return wrapper
