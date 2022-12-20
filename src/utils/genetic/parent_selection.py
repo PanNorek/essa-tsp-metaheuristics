47d6441c5d536db84f1cb8044ba30e0364199ae5
Rafal Nojek <dornier1997@gmail.com>
2022-12-21 00:36:50 +0100

    rework parent_selecttion. Add comments

--- src/utils/genetic/parent_selection.py
+++ src/utils/genetic/parent_selection.py
@@ -4,8 +4,7 @@ from .individual import Individual
 import random
 import pandas as pd
 
-
-Parents = NewType('Parents', Tuple[Individual, Individual])
+Parents = NewType("Parents", Tuple[Individual, Individual])
 
 
 class ParentSelection(ABC):
@@ -34,12 +33,14 @@ class ParentSelection(ABC):
 
 class TruncationSelection(ParentSelection):
     """
-    Elitism offspring selection
+    Truncation offspring selection
     """
 
     @staticmethod
     def select(generation: List[Individual], size: Union[int, float] = 0.5) -> Parents:
+        # calculate the number of trucated individuals
         size = ParentSelection._get_size(generation=generation, size=size)
+        # randomly select parents from the better part of the generation
         return random.sample(generation[:size], 2)
 
 
@@ -50,7 +51,9 @@ class Tournament(ParentSelection):
 
     @staticmethod
     def select(generation: List[Individual], size: Union[int, float] = 0.5) -> Parents:
+        # calculate the number of participants
         size = ParentSelection._get_size(generation=generation, size=size)
+        # select parents from 2 random batches
         batch_1 = random.sample(generation, size)
         parent_1 = sorted(batch_1, reverse=True)[0]
         batch_2 = random.sample(generation, size)
@@ -67,9 +70,15 @@ class Roulette(ParentSelection):
 
     @staticmethod
     def select(generation: List[Individual], size: None = None) -> Parents:
-        df = pd.concat([ind.to_df() for ind in generation], ignore_index=True)
+        # load individuals
+        df = pd.DataFrame.from_records(vars(o) for o in generation)
+        # calculate fitness
+        df[Individual.FITNESS] = (1 / df[Individual.DISTANCE]).astype("float64")
+        # calculate cumulative fitness
         cum_fit = df[Individual.FITNESS].cumsum()
+        # divide by total fitness
         df[Roulette.DIST_FITNESS] = cum_fit.div(df[Individual.FITNESS].sum())
+        # select parents
         mask1 = df[Roulette.DIST_FITNESS] > random.random()
         mask2 = df[Roulette.DIST_FITNESS] > random.random()
         return df[mask1].iloc[0], df[mask2].iloc[0]
